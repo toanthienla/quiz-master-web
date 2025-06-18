029047acf6446515c756db2a18d18e4b45208d53
Toan La <134460591+toanthienla@users.noreply.github.com>
2025-06-18 10:32:31 +0700

    Create Vite & Tailwind Codebase

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-600">Quiz Master</h1>
+      <p>Welcome to the Quiz Master application!</p>
+    </div>
+  );
+}
+
+export default App;
